522a94d59b0c0845f37113b43a58faa43deaa057
AnnyBarba <aebarbap@gmail.com>
2023-02-23 14:19:12 -0600

    usuario y contrasenia de conexion a la bd cambiados para el server

--- prcd/qc.php
+++ prcd/qc.php
@@ -2,9 +2,9 @@
 
 // server prueba
     $servername="localhost";
-    $database="bitacora"; //solo se quitó para conexión remota
-    $username="root";
-    $password="";
+    $database="c7bitacora"; //solo se quitó para conexión remota
+    $username="c7rfourzan";
+    $password="!nbu3ZSvZmV";
 
     $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
     $conn->set_charset("utf8");
