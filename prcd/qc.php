a985e5e03721fcc5923d8750585af4a43f1bb7eb
AnnyBarba <aebarbap@gmail.com>
2023-02-24 15:32:29 -0600

    actualizacion index para agregar archivos en impresion, publicacion y grabado de cds

--- prcd/qc.php
+++ prcd/qc.php
@@ -2,9 +2,9 @@
 
 // server prueba
     $servername="localhost";
-    $database="c7bitacora"; //solo se quitó para conexión remota
-    $username="c7rfourzan";
-    $password="!nbu3ZSvZmV";
+    $database="bitacora"; //solo se quitó para conexión remota
+    $username="root";
+    $password="";
 
     $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
     $conn->set_charset("utf8");
